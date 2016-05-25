f1ba6a8f16c72a7a36f34d1e44d3fe666fa84f90
WORK <WORK@WORK-PC>
2016-05-25 16:36:02 +0900

    /0525/0435

---
+++ springjdbc/src/myemp/MyEmpSystem.java
@@ -0,0 +1,17 @@
+package myemp;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyEmpSystem {
+	public static void main(String[] args) {
+		ApplicationContext factory = 
+			 new ClassPathXmlApplicationContext("config/scott.xml");
+		AbstractUI ui = (AbstractUI)factory.getBean("ui");
+		while(true){
+			ui.show();
+		}
+
+	}
+
+}
